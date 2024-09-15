ed160b52b1bc27a93112c452751e88febdadcfd7
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-15 19:54:40 +0530

    frontend started

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
+const dotenv = require('dotenv');
 const express = require('express');
+const { chats } = require('./data/data');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
+dotenv.config();
 
+app.get('/', (req,res)=>{
+    res.send('API is running');
+})
 
+app.get('/api/chat', (req,res)=>{
+    res.send(chats);
+})
+
+app.get('/api/chat/:id', (req,res)=>{
+    console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+})
 
 app.listen(PORT, console.log(`App started on PORT: ${PORT}`));

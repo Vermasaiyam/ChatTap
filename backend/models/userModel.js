bbd77625f5a6b84ca62037581fe5bd8f14576f33
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-15 22:27:34 +0530

    model and schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    pic:{
+        type: String,
+        required: true,
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+}, {timeStamps: true,});
+
+const User = mongoose.model( "User" , userModel);
+
+module.exports = User;

a1c70b2bc11793e40e2fe245f654cb1af3de5ecb
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-15 22:01:32 +0530

    fetching static fake chats

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route} from 'react-router-dom'
+import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path='/' component={Homepage} exact/>
+      <Route path='/chats' component={ChatPage}/>
     </div>
   );
 }

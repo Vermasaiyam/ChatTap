b5884972691fc29a5c91b640f17d60614b01d34f
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-16 10:37:51 +0530

    login and signup page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from '../components/Authentication/Login';
+import Signup from '../components/Authentication/Signup';
 
 const Homepage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box d="flex" w="100%" justifyContent="center" p={3} bg='white' m="40px 0 15px 0" borderRadius="0.5em" borderWidth="1px">
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">ChatTap</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant='soft-rounded' >
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 

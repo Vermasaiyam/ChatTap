ace1a12e6595ece6be27d956d3bbd5803a64bd5e
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-16 11:09:38 +0530

    db connected

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -3,14 +3,16 @@ import React, { useState } from 'react'
 
 const Signup = () => {
 
-  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [pic, setPic] = useState();
 
-  const handleClick = ()=> setShow(!show);
+  const handleClick1 = ()=> setShow1(!show1);
+  const handleClick2 = ()=> setShow2(!show2);
 
   const postDetails = (pics)=>{
 
@@ -33,10 +35,10 @@ const Signup = () => {
       <FormControl id="password" isRequired>
         <FormLabel>Password</FormLabel>
         <InputGroup>
-          <Input type={show ? "text" : 'password'} placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)}/>
+          <Input type={show1 ? "text" : 'password'} placeholder='Enter Your Password' onChange={(e) => setPassword(e.target.value)}/>
           <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? "Hide" : "Show"}
+            <Button h="1.75rem" size="sm" onClick={handleClick1}>
+              {show1 ? "Hide" : "Show"}
             </Button>
           </InputRightElement>
         </InputGroup>
@@ -45,10 +47,10 @@ const Signup = () => {
       <FormControl id="confirmpassword" isRequired>
         <FormLabel>Confirm Password</FormLabel>
         <InputGroup>
-          <Input type={show ? "text" : 'password'} placeholder='Confirm Your Password' onChange={(e) => setConfirmpassword(e.target.value)}/>
+          <Input type={show2 ? "text" : 'password'} placeholder='Confirm Your Password' onChange={(e) => setConfirmpassword(e.target.value)}/>
           <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? "Hide" : "Show"}
+            <Button h="1.75rem" size="sm" onClick={handleClick2}>
+              {show2 ? "Hide" : "Show"}
             </Button>
           </InputRightElement>
         </InputGroup>

484f7f318b24ea77a7332c146cf7d21100d7de02
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-17 23:19:36 +0530

    chat APIs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { registerUser, authUser, allUsers } = require('../controllers/userController');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(allUsers);
+// protect function is for checking that user is authorized for this functionalities or not
+router.route('/').post(registerUser).get(protect,allUsers);
 router.post('/login', authUser);
 
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
 
 dotenv.config();
@@ -19,6 +20,7 @@ app.get('/', (req,res)=>{
 })
 
 app.use('/api/user', userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

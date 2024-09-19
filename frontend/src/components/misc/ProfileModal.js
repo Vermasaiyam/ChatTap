d0a748f44201b5b17a11ba89f44c064dcef8e6ac
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-19 23:23:22 +0530

    group chat CRUD operations

--- frontend/src/components/misc/ProfileModal.js
+++ frontend/src/components/misc/ProfileModal.js
@@ -5,61 +5,49 @@ import React from 'react'
 const ProfileModal = ({ user, children }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <div>
+        <>
             {children ? (
                 <span onClick={onOpen}>{children}</span>
             ) : (
                 <IconButton display={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
             )}
-
             <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
-
                 <ModalOverlay />
                 <ModalContent h="410px">
-                    <Center>
-
-                        <ModalHeader
-                            fontSize="40px"
-                            fontFamily="Work sans"
-                            display="flex"
-                            justifyContent="center"
-                        >
-                            {user.name}
-                        </ModalHeader>
-                    </Center>
+                    <ModalHeader
+                        fontSize="40px"
+                        fontFamily="Work sans"
+                        display="flex"
+                        justifyContent="center"
+                    >
+                        {user.name}
+                    </ModalHeader>
                     <ModalCloseButton />
-
                     <ModalBody
                         display="flex"
                         flexDir="column"
                         alignItems="center"
                         justifyContent="space-between"
                     >
-                        <Center>
-
-                            <Image
-                                borderRadius="full"
-                                boxSize="150px"
-                                src={user.pic}
-                                alt={user.name}
-                            />
-                        </Center>
-                        <Center>
-
-                            <Text
-                                fontSize={{ base: "28px", md: "30px" }}
-                                fontFamily="Work sans"
-                            >
-                                Email: {user.email}
-                            </Text>
-                        </Center>
+                        <Image
+                            borderRadius="full"
+                            boxSize="150px"
+                            src={user.pic}
+                            alt={user.name}
+                        />
+                        <Text
+                            fontSize={{ base: "28px", md: "30px" }}
+                            fontFamily="Work sans"
+                        >
+                            Email: {user.email}
+                        </Text>
                     </ModalBody>
                     <ModalFooter>
                         <Button onClick={onClose}>Close</Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
-        </div>
+        </>
     )
 }
 
--- frontend/src/components/misc/UpdateGroupChatModal.js
+++ frontend/src/components/misc/UpdateGroupChatModal.js
@@ -4,36 +4,190 @@ import React, { useState } from 'react'
 import UserBadgeItem from '../UserAvatar/UserBadgeItem';
 import UserListItem from '../UserAvatar/UserListItem';
 import { ChatState } from '../../Context/ChatProvider';
+import axios from 'axios';
 
-const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
+const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
 
   const [groupChatName, setGroupChatName] = useState();
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [renameloading, setRenameLoading] = useState(false);
-  
+
   const toast = useToast();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const { selectedChat, setSelectedChat, user } = ChatState();
 
-  const handleRemove = ()=>{
-    
-  }
+  const handleRemove = async (user1) => {
+    if (selectedChat.groupAdmin._id !== user._id && user1._id !== user._id) {
+      toast({
+        title: "Only admins can remove someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
 
-  const handleRename = ()=>{
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `/api/chat/groupremove`,
+        {
+          chatId: selectedChat._id,
+          userId: user1._id,
+        },
+        config
+      );
 
-  }
+      user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+    setGroupChatName("");
+  };
 
-  const handleSearch = ()=>{
+  const handleRename = async () => {
+    if (!groupChatName) return;
 
-  }
+    try {
+      setRenameLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `/api/chat/rename`,
+        {
+          chatId: selectedChat._id,
+          chatName: groupChatName,
+        },
+        config
+      );
 
-  const handleAddUser = ()=>{
+      console.log(data._id);
+      // setSelectedChat("");
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setRenameLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setRenameLoading(false);
+    }
+    setGroupChatName("");
+  };
 
-  }
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleAddUser = async (user1) => {
+    if (selectedChat.users.find((u) => u._id === user1._id)) {
+      toast({
+        title: "User Already in group!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (selectedChat.groupAdmin._id !== user._id) {
+      toast({
+        title: "Only admins can add someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.put(
+        `/api/chat/groupadd`,
+        {
+          chatId: selectedChat._id,
+          userId: user1._id,
+        },
+        config
+      );
+
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+    setGroupChatName("");
+  };
 
   return (
     <>

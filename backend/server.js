efbc419180ea8f632d86b14e1307328478868d08
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-19 23:47:14 +0530

    send messages API

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.get('/', (req,res)=>{
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

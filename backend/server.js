96d41b06c73af6979ec53cf439711fe9d818a72d
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-21 00:01:56 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -33,16 +33,17 @@ app.use(errorHandler);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     );
 } else {
     app.get("/", (req, res) => {
         res.send("API is running..");
     });
 }
+
 
 
 // -----------Deployment------------------

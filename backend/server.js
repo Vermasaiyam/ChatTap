9010177d0d15933fe7fb4174ed411f9764f678f8
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-21 00:26:33 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -17,26 +17,25 @@ const app = express();
 // middlewares
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running');
-})
+// app.get('/', (req, res) => {
+//     res.send('API is running');
+// })
+
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
 
 // -----------Deployment------------------
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {  
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
     );
 } else {
     app.get("/", (req, res) => {
@@ -45,8 +44,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-
 // -----------Deployment------------------
+
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 const server = app.listen(PORT, console.log(`App started on PORT: ${PORT}`.yellow.bold));
 

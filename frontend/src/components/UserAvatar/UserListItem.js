3e5ad030e5a776834b7f4ecc31d7b15eb741d32b
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-24 00:25:12 +0530

    changes

--- frontend/src/components/UserAvatar/UserListItem.js
+++ frontend/src/components/UserAvatar/UserListItem.js
@@ -17,7 +17,7 @@ const UserListItem = ({ user, handleFunction }) => {
             color="black"
             px={3}
             py={2}
-            mb={2}
+            my={2}
             borderRadius="lg"
         >
             <Flex>
